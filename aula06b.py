d046c0c906af04f01a709b89a717970801255c14
Marcfeitosa <marcfeitosa@yahoo.com.br>
2017-08-03 15:34:04 -0300

    Add files via upload

---
+++ aula06b.py
@@ -0,0 +1,14 @@
+n = str(input('Digite um valor: '))
+print(type(n))
+
+n2 = bool(input('Digite qualquer coisa: '))
+print(n2)
+
+n3 = input('Digite ai: ')
+print(n3.isnumeric())
+
+n4 = input('Digita mais: ')
+print(n4.isalpha())
+
+n5 = input('DI-GI-TA: ')
+print(n5.isalnum())
