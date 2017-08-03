28014e587a3682165672e0133fadfc4496df1eee
Marcfeitosa <marcfeitosa@yahoo.com.br>
2017-08-03 15:25:26 -0300

    Add files via upload

---
+++ desafio 2.py
@@ -0,0 +1,4 @@
+dia=input('Qual dia você nasceu? ')
+mês=input('Em qual mês você nasceu? ')
+ano=input('Em qual ano você nasceu? ')
+print('Você nasceu no dia, ',dia,' de ',mês,' de ',ano,'. Correto?')
---
+++ desafio 3.py
@@ -0,0 +1,15 @@
+x_str=input('Primeiro número: ')
+y_str=input('Segundo número: ')
+
+x_int = int(x_str)
+y_int = int(y_str)
+
+soma = x_int + y_int
+print ('A soma é',soma)
+
+x=int(input('digite um número:'))
+y=int(input('digite outro número:'))
+
+mult = x*y
+print('A multiplicação da bagaça e:', mult)
+
